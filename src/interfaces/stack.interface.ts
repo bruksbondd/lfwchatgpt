2a2f99987b458d4f01a0709685999a068d2c1f8c
Serhii Brekhov <bruksbond@gmail.com>
2023-10-26 12:32:50 +0300

    changed next.js and typetscript

--- src/interfaces/stack.interface.ts
+++ src/interfaces/stack.interface.ts
@@ -1,21 +1,21 @@
-import { ParsedUrlQuery } from 'querystring'
+import { ParsedUrlQuery } from 'querystring';
 
 export interface Stack {
-  href?: string
-  logo: string
-  info: string
-  docs?: string[]
-  name?: string
-  hoverClass?: string
+  href?: string;
+  logo: string;
+  info: string;
+  docs?: string[];
+  name?: string;
+  hoverClass?: string;
 }
 
-export type StackKey = 'react' | 'vue' | 'svelte'
+export type StackKey = 'react' | 'next' | 'typetscript';
 
 export interface StackPageProps extends Record<string, unknown> {
-  stack: Stack
-  stackKey: string | string[] | undefined
+  stack: Stack;
+  stackKey: string | string[] | undefined;
 }
 
 export interface Params extends ParsedUrlQuery {
-  stack: string
+  stack: string;
 }
